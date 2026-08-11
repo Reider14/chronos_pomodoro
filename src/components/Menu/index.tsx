2cc07cfd85537b46bdd3c25ca71c58edbe21aeb9
haider <haider14guilherme@gmail.com>
2026-08-11 21:45:50 -0100

    useState and useEffect to chance de theme of the app

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,26 +1,64 @@
 import { HouseIcon, SettingsIcon, SunIcon, TimerIcon } from "lucide-react";
 
 import styles from "./styles.module.css";
+import { useEffect, useState } from "react";
 
-//ctrl + f to replace
+type AvailebleTheme = "dark" | "light";
 
 export function Menu() {
-  console.log(styles);
+  const [theme, setTheme] = useState<AvailebleTheme>("dark");
+
+  function handleChangeTheme(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault(); // nao seguir o link
+
+    setTheme((prevTheme) => {
+      const nextTheme = prevTheme === "dark" ? "light" : "dark";
+      return nextTheme;
+    });
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
   return (
     <nav className={styles.menu}>
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ir para home page"
+        title="Ir para home page"
+      >
         <TimerIcon />
       </a>
 
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Ver historico"
+        title="Ver historico"
+      >
         <HouseIcon />
       </a>
 
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Configuracoes"
+        title="Configuracoes"
+      >
         <SettingsIcon />
       </a>
 
-      <a className={styles.menuLink} href="#">
+      <a
+        className={styles.menuLink}
+        href="#"
+        aria-label="Mudar o tema"
+        title="Mudar o tema"
+        onClick={handleChangeTheme}
+      >
         <SunIcon />
       </a>
     </nav>
